cb8c9edbccfd51131c2b6f5cbcf193d11bde863d
Neo <petrenko-m-p@mail.ru>
2021-09-19 03:38:37 +0700

    warp

--- tasks/motion_tracking.py
+++ tasks/motion_tracking.py
@@ -6,15 +6,21 @@ from tools import *
 img = cv.imread("../imgs/lines/check.jpg")
 img = clipImg(img, 600)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-corners = cv.goodFeaturesToTrack(gray, 100, 0.03, 10)
-corners_int = np.int0(corners)
-
 canvas = np.zeros(img.shape, dtype=np.uint8)
 while True:
 
     canvas[:, :, :] = 0
+    input = np.float32([[0, 0], [img.shape[1] - 1, 0], [img.shape[1] - 1, img.shape[0] - 1], [0, img.shape[0] - 1]])
+    output = np.float32([[100,100], [500, 100], [400, 400], [200, 400]])
+    matrix = cv.getPerspectiveTransform(input, output)
+
+    imgOutput = cv.warpPerspective(img, matrix, img.shape[:2], cv.INTER_CUBIC, borderMode=cv.BORDER_CONSTANT,borderValue=(0, 0, 0))
+    cv.imshow("warp", imgOutput)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    corners = cv.goodFeaturesToTrack(gray, 100, 0.03, 10)
+    corners_int = np.int0(corners)
+
     for corner in corners_int:
         x, y = corner.ravel()
         cv.drawMarker(canvas, (x, y), (255, 100, 0))
