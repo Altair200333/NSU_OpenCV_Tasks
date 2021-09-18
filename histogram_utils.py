d232e2aedfa983ff6b36b9e1d6ee85e6ca139fea
Neo <petrenko-m-p@mail.ru>
2021-09-18 18:52:42 +0700

    histogram

--- histogram_utils.py
+++ histogram_utils.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 
+
 def prepareHistData(img, dst):
     # generate histogram
     hist, bins = np.histogram(img.ravel(), 256, [0, 256])
@@ -15,14 +16,19 @@ def prepareHistData(img, dst):
     stack[:, 1] = dst.shape[0] - stack[:, 1]
 
     return stack
-def plotBinHistogram(img, dst, color = (0, 255, 255)):
+
+
+def plotBinHistogram(img, dst, color=(0, 255, 255)):
     stack = prepareHistData(img, dst)
+    bin_w = dst.shape[1] / 255
 
-    #bruh this python loop
+    # bruh this python loop
     for i in range(256):
-        cv.rectangle(dst, np.int32((stack[i][0] - 1, stack[i][1])), np.int32((stack[i][0] + 1, dst.shape[0])), color, -1)
+        cv.rectangle(dst, (np.int32(stack[i][0] - bin_w/2), np.int32(stack[i][1])),
+                     (np.int32(stack[i][0] + bin_w/2), np.int32(dst.shape[0])), color, -1)
 
-def plotLineHistogram(img, dst, color = (0, 255, 255)):
+
+def plotLineHistogram(img, dst, color=(0, 255, 255)):
     stack = prepareHistData(img, dst)
 
     cv.polylines(dst, [np.int32(stack)], False, color)
--- tasks/noise_filtering.py
+++ tasks/noise_filtering.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import numpy as np
+
+from histogram_utils import plotBinHistogram
 from tools import *
 
 img = cv.imread("../imgs/portraits/yana.jpg")
@@ -26,7 +28,7 @@ def create_noise_img(img, param, uniform: bool = True):
 noise = create_noise_img(img, 20, False)
 
 current_method = 0
-methods_count = 2
+methods_count = 4
 
 uniform_noise = 0
 noise_scale = 20
@@ -46,9 +48,16 @@ def filter(img, method):
     filtered_img = img
     if method == 0:
         filtered_img = cv.blur(img, (3, 3))
-        return filtered_img
     if method == 1:
         filtered_img = cv.medianBlur(img, 3)
+    if method == 2:
+        kernel_size = 3
+        kernel = np.ones((kernel_size, kernel_size), dtype=np.float32)
+        kernel /= (kernel_size * kernel_size)
+        filtered_img = cv.filter2D(img, -1, kernel)
+    if method == 3:
+        filtered_img = img
+
     return filtered_img
 
 
@@ -78,12 +87,18 @@ cv.createTrackbar('ns range', controlls_window_name, 20, 150, set_noise_range)
 cv.createTrackbar('uniform', controlls_window_name, 0, 1, set_uniform)
 cv.createTrackbar('method', controlls_window_name, current_method, methods_count - 1, next_method)
 
+histogram_canvas = np.zeros((200, 400, 3), np.uint8)
+
 while True:
 
     cv.imshow("noise", cv.convertScaleAbs(noise))
 
     filtered = filter(noise, current_method)
     cv.imshow('filtered', filtered)
+
+    histogram_canvas[:, :, :] = 70
+    plotBinHistogram(filtered, histogram_canvas)
+    cv.imshow("histogram", histogram_canvas)
 
     k = cv.waitKey(1) & 0xFF
 
